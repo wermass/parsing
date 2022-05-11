77aed8549fcc3983f0f16a9a1df4a1c82681aea4
wermass <real432@rambler.ru>
2022-05-11 06:37:42 +0300

    MAGIC_SCRAPING_v1.1

--- Scraping.py
+++ Scraping.py
@@ -5,4 +5,7 @@ soup = BeautifulSoup(page.content, 'html.parser')  #  магия
 seven_day = soup.find(id="seven-day-forecast")  #  находим div (что такое див, магия)
 forecast_items = seven_day.find_all(class_="tombstone-container")  #  в div ищем контейнер, который выделяет все периоды времени (именно div контейнер)
 tonight = forecast_items[0]  #  выделяем первый прогноз
-print(tonight)
+period = tonight.find(class_="period-name").get_text()  #  из переменной tonight при помози методы .find извлекаем из класса 'period-name' информацию, .get_text() это магия, но она убирает теги.
+short_desc = tonight.find(class_='short-desc').get_text()
+temp = tonight.find(class_='temp').get_text()
+print(period, short_desc, temp, sep='\n')
